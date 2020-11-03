e626e50609b123cc5e5207906c877f2e57d1b9df
Kornel01 <42960464+Kornel01@users.noreply.github.com>
2020-11-03 13:15:37 +0100

    Add files via upload

--- insert.php
+++ insert.php
@@ -5,12 +5,12 @@ $password = "ZApgJadL9H";
 $dbname = "yjgnzoishf";
  $conn = new mysqli($servername, $username, $password, $dbname);
 
-$autor = $_POST['imie'];
+$imie = $_POST['imie'];
 $tytul = $_POST['tytul'];
 
-$sql_autor = "INSERT INTO `lib_autor`(`id_autor`, `autor`) VALUES (NULL,'$autor')";
+$sql_imie = "INSERT INTO `lib_autor`(`id_autor`, `imie`) VALUES (NULL,'$imie')";
 
-$query1 = mysqli_query($conn, $sql_autor);
+$query1 = mysqli_query($conn, $sql_imie);
 
 if($query1){
 
@@ -21,10 +21,10 @@ if($query1){
 }
 
 if($query2){
-    $id_autor = "SELECT id_autor FROM `lib_autor` WHERE autor='$autor'";
+    $id_autor = "SELECT id_autor FROM `lib_autor` WHERE imie='$imie'";
 $result1 = $conn->query($id_autor);
 while($row1 = $result1->fetch_assoc()){
-    $autorid = $row1['id_autor'];
+    $imieid = $row1['id_autor'];
 };
 
 $id_tytul = "SELECT id_tytul FROM `lib_tytul` WHERE tytul='$tytul'";
@@ -33,7 +33,7 @@ while($row2 = $result2->fetch_assoc()){
     $tytulid = $row2['id_tytul'];
 };
 
-$sql_a_t = "INSERT INTO `lib_autor_tytul`(`id_autor_tytul`, `id_autor`, `id_tytul`) VALUES (NULL,'$autorid','$tytulid')";
+$sql_a_t = "INSERT INTO `lib_autor_tytul`(`id_autor_tytul`, `id_autor`, `id_tytul`) VALUES (NULL,'$imieid','$tytulid')";
 
 $query3 = mysqli_query($conn, $sql_a_t);
 }
