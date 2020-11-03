41aad2e568fb0c8f97afaaaf22a5ce19b7401c11
Kornel01 <42960464+Kornel01@users.noreply.github.com>
2020-11-03 12:47:50 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -19,25 +19,34 @@
     </div>
     <div class="itemc">
     <?php
-    $servername = "remotemysql.com";
-    $username = "yjgnzoishf";
-    $password = "ZApgJadL9H";
-    $dbname = "yjgnzoishf";
+                $servername = "remotemysql.com";
+                $username = "yjgnzoishf";
+                $password = "ZApgJadL9H";
+                $dbname = "yjgnzoishf";
+                $conn = new mysqli($servername, $username, $password, $dbname);
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    $result = $conn -> query("select `yjgnzoishf`.`lib_autor_tytul`.`id_autor_tytul` AS `id_autor_tytul`,`yjgnzoishf`.`lib_autor`.`imie` AS `imie`,`yjgnzoishf`.`lib_tytul`.`tytul` AS `tytul` from `yjgnzoishf`.`lib_tytul` join `yjgnzoishf`.`lib_autor_tytul` join `yjgnzoishf`.`lib_autor` where `yjgnzoishf`.`lib_autor_tytul`.`id_autor` = `yjgnzoishf`.`lib_autor`.`id_autor` and `yjgnzoishf`.`lib_autor_tytul`.`id_tytul` = `yjgnzoishf`.`lib_tytul`.`id_tytul`");
-    echo("<table border='1'>");
-    while($row = $result -> fetch_assoc()){
-        echo("<tr class='myrow'>");
-        echo("<td>".$row['id_autor_tytul']."</td>"."<td>".$row['imie']."</td>"."<td>".$row['tytul']."</td>"."<td>
-        <form action='delete.php' method='POST'>
-        <input type='hidden' name='id' value=".$row['id_autor_tytul'].">
-        <input type='submit' value='delete'>
-        </form> </td>");
-        echo("</tr>");
-    }
-    echo("</table>");
-    ?>
+                $result = $conn->query("SELECT id_autor_tytul, imie, tytul FROM lib_tytul, lib_autor, lib_autor_tytul WHERE lib_autor_tytul.id_autor=lib_autor.id_autor AND lib_autor_tytul.id_tytul=lib_tytul.id_tytul");
+
+                echo("<table border=1>");
+                echo("
+                <th>id</th>
+                <th>imie</th>
+                <th>tytul</th>
+                ");
+
+                while($row = $result->fetch_assoc() ){
+                    echo("<tr>");
+                    echo("<td>".$row['id_autor_tytul']."</td>");
+                    echo("<td>".$row['imie']."</td>");
+                    echo("<td>".$row['tytul']."</td>");
+                    echo("</tr>");
+                }  
+                echo("</table>");
+
+                
+
+
+            ?>
     </div>
    
     
