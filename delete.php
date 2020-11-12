52ea6ceb39cd166a87e08b53419b224c95b2080d
Kornel01 <42960464+Kornel01@users.noreply.github.com>
2020-11-12 20:06:18 +0100

    Add files via upload

--- delete.php
+++ delete.php
@@ -4,4 +4,5 @@
  $id = $_POST['id'];
  $sql = "DELETE * from lib_autor_tytul WHERE id_autor_tytul='$id'";
  mysqli_query($conn, $sql);
+ header('Location: https://kornel1.herokuapp.com/logowanie.php');
 ?>
