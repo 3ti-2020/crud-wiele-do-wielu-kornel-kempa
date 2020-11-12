b2e7516da95779d60720a0281abd0a24cb4e5e99
Kornel01 <42960464+Kornel01@users.noreply.github.com>
2020-11-12 20:01:06 +0100

    Add files via upload

--- delete.php
+++ delete.php
@@ -2,7 +2,6 @@
  require_once "conn.php";
  $conn=mysqli_connect($servername, $username, $password, $dbname) or die("Błąd połączenia");
  $id = $_POST['id'];
- $sql = "DELETE from lib_autor_tytul WHERE id_autor_tytul='$id'";
+ $sql = "DELETE * from lib_autor_tytul WHERE id_autor_tytul='$id'";
  mysqli_query($conn, $sql);
- header('Location: https://kornel1.herokuapp.com/');
 ?>
