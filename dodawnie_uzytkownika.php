4b3fff8a602f4a1b0149c573badb57427452015b
Kornel01 <42960464+Kornel01@users.noreply.github.com>
2020-11-12 20:43:23 +0100

    Add files via upload

--- dodawnie_uzytkownika.php
+++ dodawnie_uzytkownika.php
@@ -3,7 +3,9 @@
     $conn=mysqli_connect($servername, $username, $password, $dbname) or die("Błąd połączenia");
     $haslo = $_POST['haslowlasciwy'];
     $login = $_POST['loginwlasciwy'];
-    $query = "INSERT INTO `uzytkownicy`(`id`, `login`, `haslo`) VALUES ('NULL','$login','$haslo')";
+    $query = "INSERT INTO `uzytkownicy`(`id`, `login`, `haslo`) VALUES (NULL,'$login','$haslo')";
     mysqli_query($conn, $query);
+
+    mysqli_close($conn);
     header('Location: https://kornel1.herokuapp.com/');
 ?>`
