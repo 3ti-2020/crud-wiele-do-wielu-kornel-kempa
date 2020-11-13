fcc3e21e2a8f0c071fe56b1b0ccfc1bd628564da
Kornel01 <42960464+Kornel01@users.noreply.github.com>
2020-11-13 16:08:06 +0100

    Add files via upload

--- wypozycz.php
+++ wypozycz.php
@@ -2,7 +2,8 @@
  require_once "conn.php";
  $conn=mysqli_connect($servername, $username, $password, $dbname) or die("Błąd połączenia");
  $id = $_POST['idtytul'];
- $sql = "INSERT INTO `lib_wypoz` (`id_wypoz`, `id_tytul`, `id_uzytk`, `data_wypoz`) VALUES (NULL,'$id','1',curdate()";
- mysqli_query($conn, $sql);
+ $query = "INSERT INTO `lib_wypoz` (`id_wypoz`, `id_tytul`, `id_uzytk`, `data_wypoz`) VALUES (NULL,'$id','1',curdate()";
+//  mysqli_query($conn, $sql);
+ $result=mysqli_query($conn,$query);
  header('Location: https://kornel1.herokuapp.com/logowanie.php');
 ?>
