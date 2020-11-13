30e3b421d1c6e41cb48255147a39a596dd66a04f
Kornel01 <42960464+Kornel01@users.noreply.github.com>
2020-11-13 12:58:42 +0100

    Add files via upload

--- logowanie.php
+++ logowanie.php
@@ -17,23 +17,7 @@
         <input type="submit" value="Dodaj"> <br>
         </form>
 
-        <form action="" method="post">
-            <?php
-                 require_once "conn.php";
-                 $conn=mysqli_connect($servername, $username, $password, $dbname) or die("Błąd połączenia");
-
-                 $query5 = "SELECT id_autor_tytul, imie, tytul FROM lib_tytul, lib_autor, lib_autor_tytul WHERE lib_autor_tytul.id_autor=lib_autor.id_autor AND lib_autor_tytul.id_tytul=lib_tytul.id_tytul";
-                 $result5=mysqli_query($conn,$query5);
-
-                 echo "<select name="tytul">";
-                 while($row = mysqli_fetch_row($result5))
-                 {
-                     echo "<option value'".$row[1].$row[2]"'>".$row[1].$row[2]."</option>";
-                 }
-                 echo "</select>";
-            ?>
-            <input type="submit" value="wypozycz">
-        <form>
+        
     
     </div>
   <div class="itemd">
