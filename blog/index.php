a5ba94aa2aaf244115b77dd854f6fd2a0ff58fe7
Kornel01 <42960464+Kornel01@users.noreply.github.com>
2020-12-10 12:12:55 +0100

    Add files via upload

---
+++ blog/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="itema">
+            <h1>BLOG</h1>
+        </div>
+        <div class="itemb"></div>
+        <div class="itemc">
+        <div class="menu">
+
+            <form action="wyszukiwanie.php" method="post" class='form_tag'>
+                <p>Wyszukaj po tagu:</p>
+                <p>#<input type='text' name='wyszukaj' class='szukaj'></p>
+                <input type="submit" class='szukaj' value='Wyszukaj'>
+            </form>
+
+        </div>
+        </div>
+    </div>
+    
+</body>
+</html>
